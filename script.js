707e54639dc7c77e55dc12e45c47a1dbb2ac0b69
tabka.sahar <tabkasahar3@gmail.com>
2022-04-14 16:49:53 +0200

    submit and download

--- script.js
+++ script.js
@@ -26,20 +26,20 @@ $(document).ready(function(){
       });
  });
 });
-const nom = document.getElementById('nom');
-const prénom = document.getElementById('prénom')
-const form = document.getElementById('demoForm')
-const errorElement = document.getElementById('error')
-form.addEventListener('submit',(e)=>{
-    let messages = []
-    if(nom.value && prénom.value&& mail.value==='' || nom.value&&prénom.value&& mail.value== null ){
-        messages.push("fill all blanks")
-    }
-    if(messages.length>0){
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
+// const nom = document.getElementById('nom');
+// const prénom = document.getElementById('prénom')
+// const form = document.getElementById('demoForm')
+// const errorElement = document.getElementById('error')
+// form.addEventListener('submit',(e)=>{
+//     let messages = []
+//     if(nom.value && prénom.value&& mail.value==='' || nom.value&&prénom.value&& mail.value== null ){
+//         messages.push("fill all blanks")
+//     }
+//     if(messages.length>0){
+//         e.preventDefault()
+//         errorElement.innerText = messages.join(', ')
     
-    }})
+//     }})
     myBlurFunction = function(state) {
       var containerElement = document.getElementById('main_container');
       var overlayEle = document.getElementById('popup');
@@ -64,8 +64,39 @@ form.addEventListener('submit',(e)=>{
 //     $('#body').on('input', updateEmailLink);
 //     updateEmailLink();
 // });
-var frmvalidator  = new Validator("contactform");
-frmvalidator.addValidation("name","req","Please provide your name");
-frmvalidator.addValidation("email","req","Please provide your email");
-frmvalidator.addValidation("email","email",
-  "Please enter a valid email address");
+// var frmvalidator  = new Validator("contactform");
+// frmvalidator.addValidation("name","req","Please provide your name");
+// frmvalidator.addValidation("email","req","Please provide your email");
+// frmvalidator.addValidation("email","email",
+//   "Please enter a valid email address");
+
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Thanks for your submission!";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
